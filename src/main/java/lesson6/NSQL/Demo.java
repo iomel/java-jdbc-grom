dad877e92be5b3a4f43da042153fdf7c9da7fab8
iomel <igor.omel@gmail.com>
2018-04-08 22:04:22 +0200

    lesson 6 NSQL

--- src/main/java/lesson6/NSQL/Demo.java
+++ src/main/java/lesson6/NSQL/Demo.java
@@ -1,53 +1,20 @@
 package lesson6.NSQL;
 
-import lesson6.Product;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class Demo {
     public static void main(String[] args) throws Exception {
         ProductDAO productDAO = new ProductDAO();
 
-        Product product1 = new Product();
-        product1.setName("Table");
-        product1.setDescription("grey round table");
-        product1.setPrice(70);
+        System.out.println(productDAO.findById(2L).toString());
 
-        Product product2 = new Product();
-        product2.setName(" 222 Table");
-        product2.setDescription("grey round table");
-        product2.setPrice(170);
+        for (Product product : productDAO.findByName("Table"))
+            System.out.println(product.toString());
 
-        Product product3 = new Product();
-        product3.setName("Table 333");
-        product3.setDescription("grey round table");
-        product3.setPrice(710);
+        for (Product product : productDAO.findByContainedName(" Tabl"))
+            System.out.println(product.toString());
 
-        Product product4 = new Product();
-        product4.setName(" 444 Table");
-        product4.setDescription("grey round table");
-        product4.setPrice(701);
-
-        List<Product> prodList = Arrays.asList(product2, product3, product4);
-
-        productDAO.save(product1);
-        productDAO.saveAll(prodList);
-        System.out.println("Added");
-        Thread.sleep(10000);
-
-        product1.setDescription("UPDATED");
-        product2.setDescription("UPDATED");
-        product3.setDescription("UPDATED");
-        product4.setDescription("UPDATED");
-        productDAO.update(product1);
-        productDAO.updateAll(prodList);
-        System.out.println("Updated");
-        Thread.sleep(10000);
-
-        productDAO.delete(product1);
-        productDAO.deleteAll(prodList);
-        System.out.println("Deleted");
+        for (Product product : productDAO.findByPrice(100, 70))
+            System.out.println(product.toString());
 
         System.out.println("END");
         System.exit(0);
--- src/main/java/lesson6/NSQL/ProductDAO.java
+++ src/main/java/lesson6/NSQL/ProductDAO.java
@@ -1,10 +1,9 @@
 package lesson6.NSQL;
 
-import lesson6.Product;
 import org.hibernate.HibernateException;
+import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 import java.util.List;
@@ -17,84 +16,99 @@ public class ProductDAO {
         factory = new Configuration().configure().buildSessionFactory();
     }
 
-    // save
-    public void save(Product product) {
-        Transaction transaction = null;
+
+    public Product findById(Long id) {
+        List<Product> list = null;
         try (Session session = factory.openSession()) {
-            transaction = session.beginTransaction();
-            session.save(product);
-            transaction.commit();
+            SQLQuery query = session.createSQLQuery("SELECT * FROM Product WHERE id = :id ");
+            query.setParameter("id", id);
+            query.addEntity(Product.class);
+            list = query.list();
         } catch (HibernateException e) {
-            if (transaction != null) transaction.rollback();
             e.printStackTrace();
         }
+        return list.get(0);
     }
 
-    // save All
-    public void saveAll(List<Product> products) {
-        Transaction transaction = null;
+    public List<Product> findByName(String name) {
+        List<Product> list = null;
         try (Session session = factory.openSession()) {
-            transaction = session.beginTransaction();
-            for(Product product : products)
-                session.save(product);
-            transaction.commit();
+            SQLQuery query = session.createSQLQuery("SELECT * FROM Product WHERE name = :name ");
+            query.setParameter("name", name);
+            query.addEntity(Product.class);
+            list = query.list();
         } catch (HibernateException e) {
-            if (transaction != null) transaction.rollback();
             e.printStackTrace();
         }
+        return list;
     }
 
-    // update
-    public void update(Product product) {
-        Transaction transaction = null;
+    public List<Product> findByContainedName(String name){
+        List<Product> list = null;
         try (Session session = factory.openSession()) {
-            transaction = session.beginTransaction();
-            session.merge(product);
-            transaction.commit();
+            SQLQuery query = session.createSQLQuery("SELECT * FROM Product WHERE name LIKE :name ");
+            query.setParameter("name", "%" + name + "%");
+            query.addEntity(Product.class);
+            list = query.list();
         } catch (HibernateException e) {
-            if (transaction != null) transaction.rollback();
             e.printStackTrace();
         }
+        return list;
     }
 
-    // update All
-    public void updateAll(List<Product> products) {
-        Transaction transaction = null;
+    public List<Product> findByPrice(int price, int delta){
+        List<Product> list = null;
         try (Session session = factory.openSession()) {
-            transaction = session.beginTransaction();
-            for(Product product : products)
-                session.merge(product);
-            transaction.commit();
+            SQLQuery query = session.createSQLQuery("SELECT * FROM Product WHERE price BETWEEN :mDelta AND :pDelta");
+            query.setParameter("mDelta", price - delta);
+            query.setParameter("pDelta", price + delta);
+            query.addEntity(Product.class);
+            list = query.list();
         } catch (HibernateException e) {
-            if (transaction != null) transaction.rollback();
             e.printStackTrace();
         }
+        return list;
     }
 
-    // delete
-    public void delete(Product product) {
-        Transaction transaction = null;
+    public List<Product> findByNameSortedAsc(String name){
+        List<Product> list = null;
         try (Session session = factory.openSession()) {
-            transaction = session.beginTransaction();
-            session.delete(product);
-            transaction.commit();
+            SQLQuery query = session.createSQLQuery("SELECT * FROM Product WHERE name = :name ORDER BY name ASC");
+            query.setParameter("name", name);
+            query.addEntity(Product.class);
+            list = query.list();
         } catch (HibernateException e) {
-            if (transaction != null) transaction.rollback();
             e.printStackTrace();
         }
+        return list;
     }
 
-    // delete All
-    public void deleteAll(List<Product> products) {
-        Transaction transaction = null;
+    public List<Product> findByNameSortedDesc(String name){
+        List<Product> list = null;
         try (Session session = factory.openSession()) {
-            transaction = session.beginTransaction();
-            for(Product product : products)
-                session.delete(product);
-            transaction.commit();
+            SQLQuery query = session.createSQLQuery("SELECT * FROM Product WHERE name = :name ORDER BY name DESC");
+            query.setParameter("name", name);
+            query.addEntity(Product.class);
+            list = query.list();
         } catch (HibernateException e) {
-            if (transaction != null) transaction.rollback();
             e.printStackTrace();
         }
+        return list;
     }
+
+    public List<Product> findByPriceSortedDesc(int price, int delta) {
+        List<Product> list = null;
+        try (Session session = factory.openSession()) {
+            SQLQuery query = session.createSQLQuery(
+                    "SELECT * FROM Product WHERE price BETWEEN :mDelta AND :pDelta ORDER BY price DESC");
+            query.setParameter("mDelta", price - delta);
+            query.setParameter("pDelta", price + delta);
+            query.addEntity(Product.class);
+            list = query.list();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
 }
