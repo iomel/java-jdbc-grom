183d3edf72e3c254e22d73b2e7846a25ac444d51
iomel <igor.omel@gmail.com>
2018-04-11 21:11:49 +0200

    lesson 6 HQL - methods optimization

--- src/main/java/lesson6/HQL/ProductDAO.java
+++ src/main/java/lesson6/HQL/ProductDAO.java
@@ -11,96 +11,75 @@ import java.util.List;
 
 public class ProductDAO {
     private static SessionFactory factory;
+    private String SELECT_BY_ID = "from Product where id = :id";
+    private String SELECT_BY_NAME = "from Product where name = :name";
+    private String SELECT_BY_CONTAIN_NAME = "from Product where name like :name";
+    private String SELECT_BY_NAME_ASC_SORT = "from Product where name = :name order by name asc";
+    private String SELECT_BY_NAME_DESC_SORT = "from Product where name = :name order by name desc";
+    private String SELECT_BY_PRICE_DELTA = "from Product where price between :mDelta and :pDelta";
+    private String SELECT_BY_PRICE_DELTA_DESC_SORT = "from Product where price between :mDelta and :pDelta order by price desc";
 
     public ProductDAO() {
         factory = new Configuration().configure().buildSessionFactory();
     }
 
-
-    public Product findById(Long id) {
+    private List<Product> findByOneParam (String sqlQuery, String paramName, Object paramValue){
         List<Product> list = null;
         try (Session session = factory.openSession()) {
-            Query query = session.createQuery("from Product where id = :id ");
-            query.setParameter("id", id);
+            Query query = session.createQuery(sqlQuery);
+            query.setParameter(paramName, paramValue);
             list = query.list();
         } catch (HibernateException e) {
             e.printStackTrace();
         }
-        return list.get(0);
+        return list;
+    }
+
+    private List<Product> findByTwoParam (String sqlQuery, String pName1, Object pValue1, String pName2, Object pValue2){
+        List<Product> list = null;
+        try (Session session = factory.openSession()) {
+            Query query = session.createQuery(sqlQuery);
+            query.setParameter(pName1, pValue1);
+            query.setParameter(pName2, pValue2);
+            list = query.list();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public Product findById(Long id) {
+        return findByOneParam(SELECT_BY_ID, "id", id).get(0);
     }
 
     public List<Product> findByName(String name) {
-        List<Product> list = null;
-        try (Session session = factory.openSession()) {
-            Query query = session.createQuery("from Product where name = :name ");
-            query.setParameter("name", name);
-            list = query.list();
-        } catch (HibernateException e) {
-            e.printStackTrace();
-        }
-        return list;
+        return findByOneParam(SELECT_BY_NAME, "name", name);
     }
 
     public List<Product> findByContainedName(String name){
-        List<Product> list = null;
-        try (Session session = factory.openSession()) {
-            Query query = session.createQuery("from Product where name like :name ");
-            query.setParameter("name", "%" + name + "%");
-            list = query.list();
-        } catch (HibernateException e) {
-            e.printStackTrace();
-        }
-        return list;
+        return findByOneParam(SELECT_BY_CONTAIN_NAME, "name", "%" + name + "%");
     }
 
     public List<Product> findByPrice(int price, int delta){
-        List<Product> list = null;
-        try (Session session = factory.openSession()) {
-            Query query = session.createQuery("from Product where price between :mDelta and :pDelta");
-            query.setParameter("mDelta", price - delta);
-            query.setParameter("pDelta", price + delta);
-            list = query.list();
-        } catch (HibernateException e) {
-            e.printStackTrace();
-        }
-        return list;
+        return findByTwoParam(SELECT_BY_PRICE_DELTA,
+                "mDelta", price - delta,
+                "pDelta", price + delta);
     }
 
     public List<Product> findByNameSortedAsc(String name){
-        List<Product> list = null;
-        try (Session session = factory.openSession()) {
-            Query query = session.createQuery("from Product where name = :name order by name asc");
-            query.setParameter("name", name);
-            list = query.list();
-        } catch (HibernateException e) {
-            e.printStackTrace();
-        }
-        return list;
+        return findByOneParam(SELECT_BY_NAME_ASC_SORT, "name", name);
     }
 
     public List<Product> findByNameSortedDesc(String name){
-        List<Product> list = null;
-        try (Session session = factory.openSession()) {
-            Query query = session.createQuery("from Product where name = :name order by name desc");
-            query.setParameter("name", name);
-            list = query.list();
-        } catch (HibernateException e) {
-            e.printStackTrace();
-        }
-        return list;
+        return findByOneParam(SELECT_BY_NAME_DESC_SORT, "name", name);
     }
 
     public List<Product> findByPriceSortedDesc(int price, int delta) {
-        List<Product> list = null;
-        try (Session session = factory.openSession()) {
-            Query query = session.createQuery("from Product where price between :mDelta and :pDelta order by price desc");
-            query.setParameter("mDelta", price - delta);
-            query.setParameter("pDelta", price + delta);
-            list = query.list();
-        } catch (HibernateException e) {
-            e.printStackTrace();
-        }
-        return list;
+        return findByTwoParam(SELECT_BY_PRICE_DELTA_DESC_SORT,
+                "mDelta", price - delta,
+                "pDelta", price + delta);
     }
+
+
 
 }
