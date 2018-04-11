eb827f75dd41ce81d20d3b4f3f6f47853529cb1d
iomel <igor.omel@gmail.com>
2018-04-11 20:55:02 +0200

    lesson 6 NSQL - change SQLQuery to NativeQuery

--- src/main/java/lesson6/NSQL/ProductDAO.java
+++ src/main/java/lesson6/NSQL/ProductDAO.java
@@ -1,114 +1,87 @@
 package lesson6.NSQL;
 
 import org.hibernate.HibernateException;
-import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.NativeQuery;
 
 import java.util.List;
 
 
 public class ProductDAO {
     private static SessionFactory factory;
+    private String SELECT_BY_ID = "SELECT * FROM Product WHERE id = :id";
+    private String SELECT_BY_NAME = "SELECT * FROM Product WHERE name = :name";
+    private String SELECT_BY_CONTAIN_NAME = "SELECT * FROM Product WHERE name LIKE :name";
+    private String SELECT_BY_NAME_ASC_SORT = "SELECT * FROM Product WHERE name = :name ORDER BY name ASC";
+    private String SELECT_BY_NAME_DESC_SORT = "SELECT * FROM Product WHERE name = :name ORDER BY name DESC";
+    private String SELECT_BY_PRICE_DELTA = "SELECT * FROM Product WHERE price BETWEEN :mDelta AND :pDelta";
+    private String SELECT_BY_PRICE_DELTA_DESC_SORT = "SELECT * FROM Product WHERE price BETWEEN :mDelta AND :pDelta ORDER BY price DESC";
+
 
     public ProductDAO() {
         factory = new Configuration().configure().buildSessionFactory();
     }
 
-
-    public Product findById(Long id) {
+    private List<Product> findByOneParam (String sqlQuery, String paramName, Object paramValue){
         List<Product> list = null;
         try (Session session = factory.openSession()) {
-            SQLQuery query = session.createSQLQuery("SELECT * FROM Product WHERE id = :id ");
-            query.setParameter("id", id);
+            NativeQuery query = session.createNativeQuery(sqlQuery);
+            query.setParameter(paramName, paramValue);
             query.addEntity(Product.class);
             list = query.list();
         } catch (HibernateException e) {
             e.printStackTrace();
         }
-        return list.get(0);
+        return list;
+    }
+
+    private List<Product> findByTwoParam (String sqlQuery, String pName1, Object pValue1, String pName2, Object pValue2){
+        List<Product> list = null;
+        try (Session session = factory.openSession()) {
+            NativeQuery query = session.createNativeQuery(sqlQuery);
+            query.setParameter(pName1, pValue1);
+            query.setParameter(pName2, pValue2);
+            query.addEntity(Product.class);
+            list = query.list();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+
+    public Product findById(Long id) {
+        return findByOneParam(SELECT_BY_ID, "id", id).get(0);
     }
 
     public List<Product> findByName(String name) {
-        List<Product> list = null;
-        try (Session session = factory.openSession()) {
-            SQLQuery query = session.createSQLQuery("SELECT * FROM Product WHERE name = :name ");
-            query.setParameter("name", name);
-            query.addEntity(Product.class);
-            list = query.list();
-        } catch (HibernateException e) {
-            e.printStackTrace();
-        }
-        return list;
+        return findByOneParam(SELECT_BY_NAME, "name", name);
     }
 
     public List<Product> findByContainedName(String name){
-        List<Product> list = null;
-        try (Session session = factory.openSession()) {
-            SQLQuery query = session.createSQLQuery("SELECT * FROM Product WHERE name LIKE :name ");
-            query.setParameter("name", "%" + name + "%");
-            query.addEntity(Product.class);
-            list = query.list();
-        } catch (HibernateException e) {
-            e.printStackTrace();
-        }
-        return list;
+        return findByOneParam(SELECT_BY_CONTAIN_NAME, "name", "%" + name + "%");
     }
 
     public List<Product> findByPrice(int price, int delta){
-        List<Product> list = null;
-        try (Session session = factory.openSession()) {
-            SQLQuery query = session.createSQLQuery("SELECT * FROM Product WHERE price BETWEEN :mDelta AND :pDelta");
-            query.setParameter("mDelta", price - delta);
-            query.setParameter("pDelta", price + delta);
-            query.addEntity(Product.class);
-            list = query.list();
-        } catch (HibernateException e) {
-            e.printStackTrace();
-        }
-        return list;
+        return findByTwoParam(SELECT_BY_PRICE_DELTA,
+                "mDelta", price - delta,
+                "pDelta", price + delta);
     }
 
     public List<Product> findByNameSortedAsc(String name){
-        List<Product> list = null;
-        try (Session session = factory.openSession()) {
-            SQLQuery query = session.createSQLQuery("SELECT * FROM Product WHERE name = :name ORDER BY name ASC");
-            query.setParameter("name", name);
-            query.addEntity(Product.class);
-            list = query.list();
-        } catch (HibernateException e) {
-            e.printStackTrace();
-        }
-        return list;
+        return findByOneParam(SELECT_BY_NAME_ASC_SORT, "name", name);
     }
 
     public List<Product> findByNameSortedDesc(String name){
-        List<Product> list = null;
-        try (Session session = factory.openSession()) {
-            SQLQuery query = session.createSQLQuery("SELECT * FROM Product WHERE name = :name ORDER BY name DESC");
-            query.setParameter("name", name);
-            query.addEntity(Product.class);
-            list = query.list();
-        } catch (HibernateException e) {
-            e.printStackTrace();
-        }
-        return list;
+        return findByOneParam(SELECT_BY_NAME_DESC_SORT, "name", name);
     }
 
     public List<Product> findByPriceSortedDesc(int price, int delta) {
-        List<Product> list = null;
-        try (Session session = factory.openSession()) {
-            SQLQuery query = session.createSQLQuery(
-                    "SELECT * FROM Product WHERE price BETWEEN :mDelta AND :pDelta ORDER BY price DESC");
-            query.setParameter("mDelta", price - delta);
-            query.setParameter("pDelta", price + delta);
-            query.addEntity(Product.class);
-            list = query.list();
-        } catch (HibernateException e) {
-            e.printStackTrace();
-        }
-        return list;
+        return findByTwoParam(SELECT_BY_PRICE_DELTA_DESC_SORT,
+                "mDelta", price - delta,
+                "pDelta", price + delta);
     }
 
 }
